39ddaa3015437a84ab3fe9706b1953ea5cb32005
bdanil-main <danil.bush2002@gmail.com>
2024-03-17 19:29:39 +0300

    icons added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,28 @@
-import styles from "./App.module.scss";
+import style from "./App.module.scss";
 import React from "react";
-import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Main from "./pages/Main/Main.tsx";
 import Games from "./pages/Games/Games.tsx";
+import { ReactComponent as GamesIcon } from './images/casino.svg';
 import Game_rooms from "./pages/Game_rooms/Game_rooms.tsx";
+import { ReactComponent as Game_roomsIcon } from './images/tactic.svg';
 import Profile from "./pages/Profile/Profile.tsx";
+import { ReactComponent as ProfileIcon } from './images/account_circle.svg';
 
 export const App = () => (
 	
 	<Router>
 		<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL,GRAD@400,1,0" />
-		<header className={styles.AppHeader}>
-			<nav className={styles.Nav}>
-				<Link className={styles.Logo} to="/"><img src={"https://placehold.co/50"} alt="Главная" /></Link>
-				<Link className={styles.Link} to="/games/1"><span className="material-symbols-outlined size-48">casino</span>Игры</Link>
-				<Link className={styles.Link} to="/game_rooms/1"><span className="material-symbols-outlined size-48">tactic</span>Игротеки</Link>
-				<Link className={styles.Link} to="/profile"><span className="material-symbols-outlined size-48">account_circle</span>Профиль</Link>
+		<header className={style.AppHeader}>
+			<nav className={style.Nav}>
+				<Link className={style.Logo} to="/"><img src={"https://placehold.co/50"} alt="Главная" /></Link>
+				<Link className={style.Link} to="/games/1"><GamesIcon/>Игры</Link>
+				<Link className={style.Link} to="/game_rooms/1"><Game_roomsIcon/>Игротеки</Link>
+				<Link className={style.Link} to="/profile"><ProfileIcon/>Профиль</Link>
 			</nav>
 		</header>
 
-		<main className={styles.App}>
+		<main className={style.App}>
 			<Routes>
 				<Route path="/" element={<Main />}/>
 				<Route path="/games/:id" element={<Games />}/>
@@ -28,7 +31,7 @@ export const App = () => (
 			</Routes>
 		</main>
 
-		<footer className={styles.AppFooter}>
+		<footer className={style.AppFooter}>
 			<h2>HELP US</h2>
 		</footer>
 	</Router>
