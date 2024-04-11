a648b645fff162ae76b3fa735d36d82fbb2638a1
HidasMato <shujali@chtoetozasrany.pochemuetovylezlo>
2024-04-11 21:57:17 +0300

    kosyak

--- client/src/api/http/index.ts
+++ client/src/api/http/index.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { AuthResponse } from '../models/AuthResponse';
+import { AuthResponse } from '../models/AuthResponse.ts';
 
 export const API_URL = 'http://localhost:2052'
 
